da3265d17e74dea4089195ad49ccd6bd0c8bda68
Bart Massey <bart@cs.pdx.edu>
2011-03-01 16:45:09 -0800

    added license

--- CSV.hs
+++ CSV.hs
@@ -1,4 +1,8 @@
 -- Copyright © 2011 Bart Massey
+-- [This program is licensed under the "MIT License"]
+-- Please see the file COPYING in the source
+-- distribution of this software for license terms.
+
 module CSV (readCSV, readCSV', showCSV, showCSV', 
             hPutCSV, hPutCSV', writeCSVFile, writeCSVFile')
 where
